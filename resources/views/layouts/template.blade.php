f5a639eb4c58e470f2174d43019cd6e5c0dc582c
joel-okello <okellojoelacaye@gmail.com>
2018-07-03 13:41:12 +0300

    added logout button

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -268,10 +268,18 @@
               <span aria-hidden="true">×</span>
             </button>
           </div>
-          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-body">Select "Logout"  to end your current session.</div>
           <div class="modal-footer">
             <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="login.html">Logout</a>
+             <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
           </div>
         </div>
       </div>
