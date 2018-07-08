caf3de129a5c4513b6300e8ce2b5f02d49f8a516
joel-okello <okellojoelacaye@gmail.com>
2018-07-09 02:09:56 +0300

    redirect to send money

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,6 +25,6 @@ class HomeController extends Controller
     public function index()
     {
         
-        return redirect()->route('account.index');
+        return redirect()->route('old');
     }
 }
